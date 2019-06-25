e007371995dfa348eb6d6388fbe15ada9ad120e9
HMJussani <31674866+HMJussani@users.noreply.github.com>
2019-06-25 18:50:59 -0300

    Add files via upload

---
+++ Db1StartDesafioGeografia.java
@@ -0,0 +1,28 @@
+/*
+*Faça um programa que o usuário poderá inserir um nome de cidade. O programa deverá informar:
+* Qual o estado que a cidade pertence
+* Se é capital ou não
+* O conjunto de cidades permitidas devem ser as seguintes: Curitiba, Maringá, Londrina, Pinhais,
+*  Chapecó, Joinvile, Blumenau, Florianópolis, Porto Alegre, Gramado.
+*/
+package db1startdesafiogeografia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rick
+ */
+public class Db1StartDesafioGeografia {
+  
+    public static void main(String[] args) {
+        Geografia geo = new Geografia();
+         Scanner tecl = new Scanner(System.in); //instancia scanner para o teclado;
+        System.out.println("Digite uma cidade:");
+        String cidade = geo.retornarCidades(tecl.nextLine().toUpperCase());
+        
+        System.out.println(cidade);
+       
+    }
+    
+}
